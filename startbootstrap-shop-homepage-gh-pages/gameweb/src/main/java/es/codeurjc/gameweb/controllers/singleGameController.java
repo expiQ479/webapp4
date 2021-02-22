4ced8896792db5f0494524b9a6e086043603af5e
expiQ479 <61882281+expiQ479@users.noreply.github.com>
2021-02-22 21:07:23 +0100

    Changes in navegation

--- startbootstrap-shop-homepage-gh-pages/gameweb/src/main/java/es/codeurjc/gameweb/controllers/singleGameController.java
+++ startbootstrap-shop-homepage-gh-pages/gameweb/src/main/java/es/codeurjc/gameweb/controllers/singleGameController.java
@@ -2,13 +2,13 @@ package es.codeurjc.gameweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class singleGameController {
-    @GetMapping("/GamePage")
-    public String showGame(Model model, @RequestParam String name) {
+    @RequestMapping("/GamePage/{name}")
+    public String showGame(Model model, @PathVariable String name) {
         model.addAttribute("name", name);
         return "GamePage";
     }
