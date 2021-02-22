f563959364c76a46d7bfb3a1f71413da91020e9e
expiQ479 <61882281+expiQ479@users.noreply.github.com>
2021-02-22 13:18:06 +0100

    Update controllers

--- startbootstrap-shop-homepage-gh-pages/gameweb/src/main/java/es/codeurjc/gameweb/controllers/indexController.java
+++ startbootstrap-shop-homepage-gh-pages/gameweb/src/main/java/es/codeurjc/gameweb/controllers/indexController.java
@@ -1,11 +1,11 @@
-package es.codeurjc.web;
+package es.codeurjc.gameweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class GreetingController {
+public class indexController {
     @GetMapping("/index")
     public String greeting(Model model) {
         return "index";
