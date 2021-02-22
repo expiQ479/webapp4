5c15a0b17a57a9ba67c67cf8227829c51ef47491
expiQ479 <61882281+expiQ479@users.noreply.github.com>
2021-02-22 10:14:44 +0100

    Maven Project

---
+++ startbootstrap-shop-homepage-gh-pages/gameweb/src/main/java/es/codeurjc/gameweb/GamewebApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.gameweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamewebApplication.class, args);
+	}
+
+}
---
+++ startbootstrap-shop-homepage-gh-pages/gameweb/src/test/java/es/codeurjc/gameweb/GamewebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.gameweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
