feb68628efe17379d8a17e02a8ac965ae6a58b23
Carlitosh636 <carloshervas63@hotmail.com>
2021-02-28 19:33:09 +0100

    posts update #1

--- Backend/gameweb/src/main/java/es/codeurjc/gameweb/models/UpdatePost.java
+++ Backend/gameweb/src/main/java/es/codeurjc/gameweb/models/UpdatePost.java
@@ -1,15 +1,18 @@
 package es.codeurjc.gameweb.models;
 
 import java.sql.Date;
-import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 
 public class UpdatePost extends Post {
-    public List<String> listOfUpdates;
-    public UpdatePost(String title, Date creationDate, Date updateDate, Game fromGame, List<String> listOfUpdates){
+    public String prevText;
+    public ArrayList<String> listOfUpdates;
+    public UpdatePost(String title, Date creationDate, Date updateDate, Game fromGame, ArrayList<String> listOfUpdates,String prevText){
         super(title, creationDate, updateDate, fromGame);
+        this.prevText=prevText;
         this.listOfUpdates=listOfUpdates;
+        this.prevText=prevText;
     }
 
     
