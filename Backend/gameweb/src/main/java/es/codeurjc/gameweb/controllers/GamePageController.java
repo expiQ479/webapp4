41ff92d0c9552c2d94c70d083b2190c87bb8db40
Carlitosh636 <carloshervas63@hotmail.com>
2021-03-01 17:41:02 +0100

    sub to game button #1

--- Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/GamePageController.java
+++ Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/GamePageController.java
@@ -1,19 +1,27 @@
 package es.codeurjc.gameweb.controllers;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import es.codeurjc.gameweb.models.*;
 
 @Controller
 public class GamePageController {
-    /*@Autowired
-    private User user;*/
-    @RequestMapping(value="/subButton")
-    public String subButton(){
-        /*ArrayList<Game> games=user.getMyGames();
-        for(int i=0;i<games.size();i++){
-            System.out.println(games.get(i));
-        }*/
-        
-        return "index";
-    }
+    @Autowired
+    private CommonFunctions commonFunctions;
+
+    @RequestMapping("/GamePage/{name}/subButton")
+    public String subButton(@PathVariable String name){    
+        ArrayList<Game> someGames=new ArrayList<Game>();
+        someGames.add(new Game(name, null, null));
+        commonFunctions.getU().setMyGames(someGames);
+        for(int i=0;i<someGames.size();i++){
+            System.out.println(commonFunctions.getU().getMyGames().get(i).gameTitle);
+        }  
+        return "Suscripciones";  
+    }   
 }
