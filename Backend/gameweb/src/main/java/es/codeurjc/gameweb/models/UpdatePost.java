dad8ca24201ccf7056b401730d8c85c68f80f37b
eduardosoy <61882315+eduardosoy@users.noreply.github.com>
2021-03-01 11:10:15 +0100

    not used imports deleted

--- Backend/gameweb/src/main/java/es/codeurjc/gameweb/models/UpdatePost.java
+++ Backend/gameweb/src/main/java/es/codeurjc/gameweb/models/UpdatePost.java
@@ -3,8 +3,6 @@ package es.codeurjc.gameweb.models;
 import java.sql.Date;
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
-
 public class UpdatePost extends Post {
     public List<String> listOfUpdates;
     public UpdatePost(String title, Date creationDate, Date updateDate, Game fromGame, List<String> listOfUpdates){
