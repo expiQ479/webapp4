c25d510bcf8cb06d1725d9cf1c7fabb3446c498d
Carlitosh636 <carloshervas63@hotmail.com>
2021-03-03 18:24:53 +0100

    Error page template and sub button hotfix

--- Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/NavigationController.java
+++ Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/NavigationController.java
@@ -3,7 +3,12 @@ package es.codeurjc.gameweb.controllers;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +19,7 @@ import es.codeurjc.gameweb.services.GamePostService;
 import es.codeurjc.gameweb.services.ImageService;
 
 @Controller
-public class NavigationController{
+public class NavigationController implements ErrorController{
     @Autowired
     private CommonFunctions commonFunctions;
     @Autowired
@@ -99,6 +104,26 @@ public class NavigationController{
     public String showListGames(Model model) {
         commonFunctions.getSession(model);
         return "gameList";
+    }
+    @RequestMapping("/error")
+public String handleError(HttpServletRequest request) {
+    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    
+    if (status != null) {
+        Integer statusCode = Integer.valueOf(status.toString());
+    
+        if(statusCode == HttpStatus.NOT_FOUND.value()) {
+            return "error";
+        }
+        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            return "error";
+        }
+    }
+    return "error";
+}
+    @Override
+    public String getErrorPath() {
+        return null;
     }
     
 }
