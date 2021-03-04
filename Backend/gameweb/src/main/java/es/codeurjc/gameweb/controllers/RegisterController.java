4df351c29e399e89ca85fa2205299effcf54022b
expiQ479 <61882281+expiQ479@users.noreply.github.com>
2021-03-04 01:22:35 +0100

    Register #1

--- Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/RegisterController.java
+++ Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/RegisterController.java
@@ -1,9 +1,31 @@
 package es.codeurjc.gameweb.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import es.codeurjc.gameweb.models.User;
+import es.codeurjc.gameweb.services.UserService;
 
 @Controller
 public class RegisterController {
-    
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CommonFunctions commonFunctions;
+
+    @PostMapping("/Registrar")
+    public String newUser(Model model, User user) throws IOException {
+        user.setLogged(true);
+        user.setAdmin(false);
+        commonFunctions.getSession(model);
+        userService.save(user);
+        return "index";
+    }
+
 }
