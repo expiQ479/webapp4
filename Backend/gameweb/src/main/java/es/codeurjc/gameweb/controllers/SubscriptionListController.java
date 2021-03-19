e0d161b53f8584ef574a13a6d08a7af50bb4a4b8
expiQ479 <61882281+expiQ479@users.noreply.github.com>
2021-03-19 20:33:05 +0100

    Subscription updated

--- Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/SubscriptionListController.java
+++ Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/SubscriptionListController.java
@@ -1,11 +1,14 @@
 package es.codeurjc.gameweb.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import es.codeurjc.gameweb.models.Game;
 import es.codeurjc.gameweb.services.GamePostService;
 import es.codeurjc.gameweb.services.UserService;
 
@@ -20,16 +23,20 @@ public class SubscriptionListController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/Subscriptions/{gameTitle}")
-    public String eliminarSubs(Model model, @PathVariable String gameTitle){
+    @RequestMapping("/Subscriptions/{id}")
+    public String eliminarSubs(Model model, @PathVariable Long id){
+        ArrayList<Game> myGames = new ArrayList<>();
         for(int i=0; i<commonFunctions.getU().getMyGames().size(); i++){
-            if(gamePostService.findById(commonFunctions.getU().getMyGames().get(i)).get().getGameTitle().equals(gameTitle)){
+            if(commonFunctions.getU().getMyGames().get(i).equals(id)){
                 commonFunctions.getU().getMyGames().remove(commonFunctions.getU().getMyGames().get(i));
-                model.addAttribute("listaSubs",commonFunctions.getU().getMyGames());
                 commonFunctions.getSession(model);
                 userService.save(commonFunctions.getU());
             }
+            else{
+                myGames.add(gamePostService.findById(commonFunctions.getU().getMyGames().get(i)).get());
+            }
         }
+        model.addAttribute("listaSubs", myGames);
         commonFunctions.getSession(model);
         return "Subscriptions";
     } 
