89700661b4d85d9ae999614d513933645f2b6510
eduardosoy <61882315+eduardosoy@users.noreply.github.com>
2021-03-21 14:08:16 +0100

    7 classes with security

--- Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/LoginController.java
+++ Backend/gameweb/src/main/java/es/codeurjc/gameweb/controllers/LoginController.java
@@ -1,12 +1,16 @@
 package es.codeurjc.gameweb.controllers;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,22 +24,29 @@ public class LoginController {
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private CommonFunctions commonFunctions;
-
     @Autowired
 	private GamePostService gamePostService;
+    @ModelAttribute
+	public void addAttributes(Model model, HttpServletRequest request) {
+
+		Principal principal = request.getUserPrincipal();
+
+		if (principal != null) {
+
+			model.addAttribute("logged", true);
+			model.addAttribute("userName", principal.getName());
+			model.addAttribute("admin", request.isUserInRole("ADMIN"));
+
+		} else {
+			model.addAttribute("logged", false);
+		}
+	}
 
     @PostMapping("/Index")
     public String IniciarSesion(Model model, @RequestParam String name, @RequestParam String password) throws IOException {
         List<User> userList = userService.findAll();
         for(int i=0; i<userList.size(); i++){
             if(userList.get(i).getInfo().equals(name) && userList.get(i).getPassword().equals(password)){
-                User user = userList.get(i);
-                user.setLogged(true);
-                commonFunctions.setU(user);
-                commonFunctions.getSession(model);
                 model.addAttribute("games", gamePostService.findAll());
                 ArrayList<Game> toShow=gamePostService.getNumberOfGames(3, gamePostService.findBestRatedGames());                 
                 model.addAttribute("selectedList",toShow);
@@ -44,7 +55,6 @@ public class LoginController {
                 return "index";
             }
         }
-        commonFunctions.getSession(model);
         return "LogInPage";
     }
 
