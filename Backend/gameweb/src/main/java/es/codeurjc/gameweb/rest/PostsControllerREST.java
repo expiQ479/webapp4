8e6f7c542e5710aae7e9787eeda2811a5083b886
Carlitosh636 <carloshervas63@hotmail.com>
2021-04-02 10:29:16 +0200

    craete & delete post REST

--- Backend/gameweb/src/main/java/es/codeurjc/gameweb/rest/PostsControllerREST.java
+++ Backend/gameweb/src/main/java/es/codeurjc/gameweb/rest/PostsControllerREST.java
@@ -1,40 +1,28 @@
 package es.codeurjc.gameweb.rest;
+import java.net.URI;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Optional;
-import java.security.Principal;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
-import org.hibernate.engine.jdbc.BlobProxy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpHeaders;
 
 import es.codeurjc.gameweb.models.*;
 import es.codeurjc.gameweb.repositories.PostRepository;
 import es.codeurjc.gameweb.services.*;
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 import javax.servlet.http.HttpServletRequest;
 @RestController
+@RequestMapping("/api")
 public class PostsControllerREST {
     @Autowired
     private PostService pService;    
@@ -57,4 +45,24 @@ public class PostsControllerREST {
             return ResponseEntity.notFound().build();
         }
     }
+    @PostMapping("/listPosts/{gameID}")
+    public ResponseEntity<Post> createPost(@ModelAttribute Post post,@PathVariable long gameID){
+        post.setFromGame(gamePostService.findById(gameID).get());
+        post.setTheType(PostType.Guides);
+        pService.save(post);
+        URI location=fromCurrentRequest().path("/listPosts/{id}").buildAndExpand(post.getId()).toUri();
+        return ResponseEntity.created(location).body(post);
+    }
+    @DeleteMapping("/listPosts/{id}")
+    public ResponseEntity<Post> deletePost(@PathVariable long id){
+        Post post=pService.findById(id).get();
+        if(post!=null){
+            pService.deleteById(id);
+            return ResponseEntity.ok(post);
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
 }
