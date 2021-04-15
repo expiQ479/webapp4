2c74939fb4fe0029f3e1add617b7f3d52626f114
Carlitosh636 <carloshervas63@hotmail.com>
2021-04-15 16:51:35 +0200

    properParameters #2

--- Backend/gameweb/src/main/java/es/codeurjc/gameweb/rest/AdminRestController.java
+++ Backend/gameweb/src/main/java/es/codeurjc/gameweb/rest/AdminRestController.java
@@ -38,28 +38,28 @@ public class AdminRestController {
     }*/
     @JsonView(gameBasico.class)
     @GetMapping("/adminUpdate/genres")
-    public Collection<Game> getGamesByGenre(@RequestParam int num) {
+    public Collection<Game> getGamesByGenre(@RequestParam String genre) {
         Genres gameGenre;
-        switch(num){
-            case 1:
+        switch(genre){
+            case "Horror":
                 gameGenre=Genres.Horror;
                 break;
-            case 2:
+            case "Action":
                 gameGenre=Genres.Action;
                 break;
-            case 3:
+            case "Shooter":
                 gameGenre=Genres.Shooter;
                 break;
-            case 4:
+            case "Platformer":
                 gameGenre=Genres.Platformer;
                 break;
-            case 5:
+            case "Sports":
                 gameGenre=Genres.Sports;
                 break;
-            case 6:
+            case "Puzzles":
                 gameGenre=Genres.Puzzles;
                 break;
-            case 7:
+            case "RPG":
                 gameGenre=Genres.RPG;
                 break;
             default:
