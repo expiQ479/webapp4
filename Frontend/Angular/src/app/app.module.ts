8c9676547a39a2989f52aee656ef010b095b3ca1
eduardosoy <61882315+eduardosoy@users.noreply.github.com>
2021-04-22 15:42:16 +0200

    first conection to rest

--- Frontend/Angular/src/app/app.module.ts
+++ Frontend/Angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -8,7 +9,7 @@ import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [AppComponent,HeaderComponent,FooterComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent] 
 })
